9042df877088dccec89f1cafcb4d49b0cfe0a640
DaliaAbdelghani <engdna87@gmail.com>
2021-03-16 17:52:09 +0200

    update

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 'use strict';
 
+function getAge(){
+  return Math.floor(Math.random() * (24 - 18 + 1) + 18);
+}
+
+let studentInfo =[];
+let studentsData = ['id','Name','Email','Mobile','age','Tuition'];
+let inputs = document.getElementById('form');
+let total = 0;
+let ids = 1 ;
+
 function Student (email, mobile,tuition ) {
   this.email=email;
   this.mobile=mobile;
-  this.tuition=tuition;
+  this.tuition=parseInt(tuition);
   this.name=email.split('@')[0];
-  this.age=0;
-  this.id=0;
-  this.studentInfo = [];
+  this.age=getAge();
+  this.id=ids++;
 };
 
-Student.prototype.getAge = function (){
-  this.age=Math.floor(Math.random() * 24) + 18;
-};
+function load(){
+  let users =localStorage.getItem('user');
+  if(users){
+    studentInfo=JSON.parse(users);
+    ids = studentInfo.length + 1;
+    studentInfo.forEach(student => {
+      createStudentRecord(student);
+    });
 
-let studentsData = ['id','Name','Email','Mobile','age','Tuition'];
+  }
+}
 
 let container = document.getElementById('tableArea');
 let myTable = document.createElement('table');
@@ -25,18 +40,17 @@ let headerRow = document.createElement('tr');
 header.appendChild(headerRow);
 let body = document.createElement('tbody');
 myTable.appendChild(body);
-let row = document.createElement('tr');
-body.appendChild(row);
+
 let footer = document.createElement('tfoot');
 myTable.appendChild(footer);
+footer.textContent='Total : '+total;
 
 for (let i=0; i<studentsData.length;i++){
   let headerData = document.createElement('td');
   headerData.textContent= studentsData[i];
   headerRow.appendChild(headerData);
 }
-
-let inputs = document.getElementById('form');
+load();
 
 inputs.addEventListener ('submit', getStudentInfo);
 
@@ -45,36 +59,48 @@ function getStudentInfo (event) {
   let email= event.target.email.value;
   let mobile= event.target.mobile.value;
   let tuition= event.target.tuition.value;
+ 
+  let student = new Student(email, mobile,tuition);
 
-  let rowData1 = document.createElement('td');
-  row.appendChild(rowData1);
-  rowData1.textContent= 'id';
+  createStudentRecord(student);
 
-  let rowData2 = document.createElement('td');
-  row.appendChild(rowData2);
-  let name =email.split('@')[0];
-  rowData2.textContent= name;
 
-  let rowData3 = document.createElement('td');
-  rowData3.textContent= email;
-  row.appendChild(rowData3);
-
-  let rowData4 = document.createElement('td');
-  rowData4.textContent= mobile;
-  row.appendChild(rowData4);
-
-  let rowData5 = document.createElement('td');
-  rowData5.textContent= 'age';
-  row.appendChild(rowData5);
-
-  let rowData6 = document.createElement('td');
-  rowData6.textContent= tuition;
-  row.appendChild(rowData6);
-
-let student = new Student (email,mobile,tuition);
-student.getAge();
+  studentInfo.push(student);
+  localStorage.setItem('user', JSON.stringify(studentInfo));
 
 }
 
-//localStorage.setItem('user', JSON.stringify(newStudent));
-//Student = JSON.parse(localStorage.getItem('user'));
+function createStudentRecord(student) {
+  let row = document.createElement('tr');
+  body.appendChild(row);
+
+  let rowData1 = document.createElement('td');
+  row.appendChild(rowData1);
+  rowData1.textContent = student.id;
+
+  let rowData2 = document.createElement('td');
+  row.appendChild(rowData2);
+
+  rowData2.textContent = student.name;
+
+  let rowData3 = document.createElement('td');
+  rowData3.textContent = student.email;
+  row.appendChild(rowData3);
+
+  let rowData4 = document.createElement('td');
+  rowData4.textContent = student.mobile;
+  row.appendChild(rowData4);
+
+  let rowData5 = document.createElement('td');
+
+  rowData5.textContent = student.age;
+  row.appendChild(rowData5);
+
+  let rowData6 = document.createElement('td');
+  rowData6.textContent = student.tuition;
+  row.appendChild(rowData6);
+  total +=student.tuition;
+
+  footer.textContent='Total : '+total;
+}
+
